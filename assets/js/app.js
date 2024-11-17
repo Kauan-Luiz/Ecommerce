3fc2ff7453391be50bef5f48adf1e3fb21927b7e
Kauan-Luiz <kl321491@gmail.com>
2024-11-17 12:23:36 -0300

    html, css e Javascript do ecommerce

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+let MenuItens = document.getElementById("MenuItens");
+
+MenuItens.style.maxHeight = "0px";
+
+function menucelular(){
+    if(MenuItens.style.maxHeight == "0px"){
+        MenuItens.style.maxHeight = "200px"
+    }else{
+        MenuItens.style.maxHeight = "0px"
+    }
+    
+}
